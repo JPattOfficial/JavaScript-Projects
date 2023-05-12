918feebad51c5a7030a7d3a2bd8ddfdf2d539dca
JPattOfficial <HeSpeaks7@gmail.com>
2023-05-11 17:48:02 -0700

    Update

---
+++ ToDo_App/JS/todo.js
@@ -0,0 +1,32 @@
+/*This function gets the task from input*/
+function get_todos() {
+    var todos = new Array;
+    var todos_str = localStorage.getItem('todo');
+    if (todos_str !== null) {
+        todos = JSON.parse(todos_str);
+    }
+    return todos;
+}
+
+/*This function will add the inputed task to the get_todos function array,*/
+function add() {
+    var task = document.getElementById('task').value;
+    var todos = get_todos();
+    todos.push(task);
+    localStorage.setItem('todo', JSON.stringify(todos));
+    show();
+    return false;
+}
+
+function show() {
+    var todos = get_todos();
+    var html = '<ul>';
+    for (var i = 0; i < todos.length; i++) {
+        html += '<li>' + todos[i] + '<button class="remove" id="' + i + '">x</button></li>';
+    };
+    html += '</ul>';
+    document.getElementById('todos').innerHTML = html;
+}
+
+document.getElementById('add').addEventListener('click', add);
+show();
